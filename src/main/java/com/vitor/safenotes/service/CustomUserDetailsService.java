268da1a57ff7ca487a37b4e92680cfe462dc9f93
Vitor Lopes <vitorlopeslopes193@gmail.com>
2025-11-20 02:11:53 -0300

    configuração do serviço de login para buscar usuários no Postgres

---
+++ src/main/java/com/vitor/safenotes/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.vitor.safenotes.service;
+
+import com.vitor.safenotes.model.User;
+import com.vitor.safenotes.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
