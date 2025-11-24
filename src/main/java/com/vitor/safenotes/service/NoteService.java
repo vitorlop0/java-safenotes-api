05f50a70759b8e72fb5361357ce3d74fe8965fd5
Vitor Lopes <vitorlopeslopes193@gmail.com>
2025-11-23 21:12:17 -0300

    garantir vinculo entre usuario autenticado e nova nota

--- src/main/java/com/vitor/safenotes/service/NoteService.java
+++ src/main/java/com/vitor/safenotes/service/NoteService.java
@@ -1,7 +1,10 @@
 package com.vitor.safenotes.service;
 
 import com.vitor.safenotes.model.Note;
+import com.vitor.safenotes.model.User;
 import com.vitor.safenotes.repository.NoteRepository;
+import com.vitor.safenotes.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +13,23 @@ import java.util.List;
 public class NoteService {
 
     private NoteRepository noteRepository;
+    private UserRepository userRepository;
 
-    public NoteService(NoteRepository noteRepository) {
+    public NoteService(NoteRepository noteRepository, UserRepository userRepository) {
         this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
+
+    }
+
+    public User getLoggedUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
     }
 
     public Note createNote(Note note) {
+        User user = getLoggedUser();
+        note.setOwner(user);
         return noteRepository.save(note);
     }
 
